84fbcf0a68746758e85c31da721fc56a3484ac0c
Oleg <kharenkoleg@gmail.com>
2023-11-08 18:58:25 +0100

    bueno, vamos arrancando

--- DAM2_M06_UF2_DAO_Pattern_Java/src/test/java/dao/DAOException.java
+++ DAM2_M06_UF2_DAO_Pattern_Java/src/test/java/dao/DAOException.java
@@ -1,10 +1,20 @@
 package dao;
 
-public class DAOException {
+public class DAOException extends Exception{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public DAOException() {
+		super();
 	}
-
+	
+	public DAOException(String message) {
+		super(message);
+	}
+	
+	public DAOException(Throwable cause) {
+		super(cause);
+	}
+	
+	public DAOException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }

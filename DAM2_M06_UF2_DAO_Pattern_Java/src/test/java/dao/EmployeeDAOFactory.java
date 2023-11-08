71c7d2635839a8545a11732170267d2b4dca3104
Oleg <kharenkoleg@gmail.com>
2023-11-08 18:53:12 +0100

    first comm

---
+++ DAM2_M06_UF2_DAO_Pattern_Java/src/test/java/dao/EmployeeDAOFactory.java
@@ -0,0 +1,10 @@
+package dao;
+
+public class EmployeeDAOFactory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DAM2_M06_UF2_DAO_Pattern_Java/src/test/java/dao/EmployeeDAOJDBCImpl.java
@@ -0,0 +1,10 @@
+package dao;
+
+public class EmployeeDAOJDBCImpl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

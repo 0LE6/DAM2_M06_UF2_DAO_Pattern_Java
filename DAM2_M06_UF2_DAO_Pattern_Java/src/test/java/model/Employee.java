2d9a70804c3298f27a8c2b98d91da90b784c14cb
Oleg <kharenkoleg@gmail.com>
2023-11-09 16:31:24 +0100

    Employee

--- DAM2_M06_UF2_DAO_Pattern_Java/src/test/java/model/Employee.java
+++ DAM2_M06_UF2_DAO_Pattern_Java/src/test/java/model/Employee.java
@@ -1,10 +1,125 @@
 package model;
 
+import java.util.Date;
+
 public class Employee {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private int employeeId;
+	private String first_name; // can be null
+	private String last_name;
+	private String email;
+	private String phone_number; // can be null
+	private Date hire_date;
+	private String job_id;
+	private float salary;
+	private float commission_pct;
+	private int manager_id;
+	private int department_id;
+	
+	public Employee() {}
+	
+	public Employee(int employeeId, String first_name, String last_name, String email, String phone_number,
+			Date hire_date, String job_id, float salary, float commission_pct, int manager_id, int department_id) {
+		super();
+		this.employeeId = employeeId;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+		this.phone_number = phone_number;
+		this.hire_date = hire_date;
+		this.job_id = job_id;
+		this.salary = salary;
+		this.commission_pct = commission_pct;
+		this.manager_id = manager_id;
+		this.department_id = department_id;
+	}
+		
+	public int getEmployeeId() {
+		return employeeId;
+	}
 
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone_number() {
+		return phone_number;
+	}
+
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+
+	public Date getHire_date() {
+		return hire_date;
+	}
+
+	public void setHire_date(Date hire_date) {
+		this.hire_date = hire_date;
+	}
+
+	public String getJob_id() {
+		return job_id;
+	}
+
+	public void setJob_id(String job_id) {
+		this.job_id = job_id;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+	public float getCommission_pct() {
+		return commission_pct;
+	}
+
+	public void setCommission_pct(float commission_pct) {
+		this.commission_pct = commission_pct;
+	}
+
+	public int getManager_id() {
+		return manager_id;
+	}
+
+	public void setManager_id(int manager_id) {
+		this.manager_id = manager_id;
+	}
+
+	public int getDepartment_id() {
+		return department_id;
+	}
+
+	public void setDepartment_id(int department_id) {
+		this.department_id = department_id;
 	}
 
 }

3febac5d225881bb6a3feceedca6e88b80a0ed5a
Oleg <kharenkoleg@gmail.com>
2023-11-09 15:30:31 +0100

    añadida la interficie EmployeeDAO

--- DAM2_M06_UF2_DAO_Pattern_Java/src/test/java/dao/EmployeeDAO.java
+++ DAM2_M06_UF2_DAO_Pattern_Java/src/test/java/dao/EmployeeDAO.java
@@ -1,6 +1,12 @@
 package dao;
 
+import model.Employee;
+
 public interface EmployeeDAO {
 
-	
+	public void add(Employee empl);
+	public void update(Employee empl);
+	public void delete(int id);
+	public Employee findById(int id);
+	public Employee[] getAllEmployees();
 }
